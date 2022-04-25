a9d9782bb4fd52e9d186033463c61311bca7ea1d
Felipe Castillo <crfcastilolu@gmail.com>
2022-04-25 14:16:17 -0500

    feat(Function): Slider method added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,41 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+import * as $ from 'jquery';
+import 'slick-carousel';
+import 'slick-carousel/slick/slick.css';
+/**
+ * Creates a slider with slick plugin
+ * @param {string} selector - use your class to apply slick
+ * @param {object} options  - configure options in the slider
+ * @example
+ * slicker('.movie__gallery', {arrows: false});
+ * @returns void
+ *
+ */
+const slider = (selector: string, options: object = {}) => {
+  $(selector).slick({
+    arrows: true,
+    centerMode: true,
+    centerPadding: '40px',
+    responsive: [
+      {
+        breakpoint: 2560,
+        settings: {
+          arrows: true,
+          centerMode: false,
+          centerPadding: '40px',
+          slidesToShow: 8,
+        },
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3,
+        },
+      },
+    ],
+    ...options,
+  });
+};
+export default slider;
